5149604a6d6d469850c99587298dc2f3f803e8a0
魏如博 <weirubo@le.com>
2018-10-22 15:18:17 +0800

    :tada: csv file to array.

---
+++ README.md
@@ -0,0 +1,31 @@
+# CSV TO ARRAY
+
+CSV FILE TO ARRAY.
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require frankphper/csvtoarray
+```
+
+## Usage
+
+``` php
+$arr = new \Frankphper\Csv\csvToArray\csvToArray($csvFile);
+print_r($arr->csvToArray());
+```
+
+## Testing
+
+Tests unavailable.
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
+
---
+++ src/csvToArray/csvToArray.php
@@ -0,0 +1,31 @@
+<?php
+namespace Frankphper\Csv\csvToArray;
+class csvToArray
+{
+    protected $csvFile;
+    protected $data = [];
+
+    public function __construct($file) {
+        $this->csvFile = $file;
+    }
+
+    public function csvData()
+    {
+        $handle = fopen($this->csvFile, "r");
+        if($handle === false) {
+
+        }
+        while(feof($handle) === false) {
+            yield fgetcsv($handle);
+        }
+        fclose($handle);
+    }
+    
+    public function csvToArray()
+    {
+        foreach($this->csvData($this->csvFile) as $row) {
+            array_push($this->data, $row);
+        }
+        return $this->data;
+    }
+}
